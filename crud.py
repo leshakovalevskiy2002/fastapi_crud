c49cf9c1d4773169faf814684b3f3938381016be
leshakovalevskiy2002 <lesha_programmer02_gt@mail.ru>
2026-01-13 20:11:24 +0300

    feature: use pydantic in project to validation data

--- crud.py
+++ crud.py
@@ -1,5 +1,7 @@
-from fastapi import FastAPI, HTTPException, status, Body
+from fastapi import FastAPI, HTTPException, status
+from pydantic import BaseModel, Field
 from typing import Annotated
+from datetime import datetime
 
 
 app = FastAPI(
@@ -20,66 +22,100 @@ app = FastAPI(
 )
 
 
-posts_db: dict[int, str] = {
-    1: "My first post to work with FastAPI"
-}
+class PostUpsert(BaseModel):
+    title: Annotated[
+        str,
+        Field(
+            min_length=5,
+            max_length=100
+        )
+    ]
+    slug: Annotated[
+        str,
+        Field(
+            max_length=100,
+            pattern="^[-_a-zA-Z0-9]*$"
+        )
+    ]
+    content: Annotated[str, Field(max_length=1000)]
+    is_draft: Annotated[bool, Field(default=False)]
 
 
-@app.get("/posts", tags=["posts"])
-async def read_posts() -> dict[int, str]:
+class Post(BaseModel):
+    id: Annotated[int, Field(gt=0)]
+    title: str
+    slug: str
+    content: str
+    date_created: datetime
+    date_updated: Annotated[
+        datetime | None,
+        Field(default_factory=datetime.now)
+    ]
+    is_draft: bool
+
+
+posts_db: list[Post] = []
+
+
+@app.get("/posts", tags=["posts"],
+         response_model=list[Post])
+async def read_posts() -> list[Post]:
     return posts_db
 
 
-@app.get("/posts/{post_id}", tags=["posts"])
-async def read_post(post_id: int) -> str:
-    try:
-        return posts_db[post_id]
-    except:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail="Post not found"
-        )
+@app.get("/posts/{post_id}", tags=["posts"],
+         response_model=Post)
+async def read_post(post_id: int) -> Post:
+    for post in posts_db:
+        if post.id == post_id:
+            return post
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="Post not found"
+    )
 
 
-@app.post("/posts", status_code=status.HTTP_201_CREATED, tags=["posts"])
-async def create_post(
-        post_message: Annotated[
-            str,
-            Body()
-        ]
-) -> str:
-    index = max(posts_db) + 1 if posts_db else 1
-    posts_db[index] = post_message
-    return "Post created!"
+@app.post("/posts", status_code=status.HTTP_201_CREATED,
+          tags=["posts"], response_model=Post)
+async def create_post(new_post: PostUpsert) -> Post:
+    pk = max(post.id for post in posts_db) + 1 if posts_db else 1
+    post = Post(id=pk, **new_post.model_dump(),
+                date_created=datetime.now(), date_updated=None)
+    posts_db.append(post)
+    return post
 
 
-@app.put("/posts/{post_id}", tags=["posts"])
+@app.put("/posts/{post_id}", tags=["posts"],
+         response_model=Post)
 async def update_post(
         post_id: int,
-        post_message : Annotated[
-            str,
-            Body()
-        ]
-) -> str:
-    if post_id not in posts_db:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail="Post not found"
-        )
-    posts_db[post_id] = post_message
-
-    return f"Post {post_id} was successfully updated"
+        updated_post: PostUpsert
+) -> Post:
+    for i, post in enumerate(posts_db):
+        if post.id == post_id:
+            new_post = Post(id=post_id, date_created=post.date_created, **updated_post.model_dump())
+            posts_db[i] = new_post
+            return new_post
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="Post not found"
+    )
 
 
-@app.delete("/posts/{post_id}", status_code=status.HTTP_200_OK, tags=["posts"])
-async def delete_post(post_id: int) -> str:
-    if post_id not in posts_db:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
-    del posts_db[post_id]
+@app.delete("/posts/{post_id}", status_code=status.HTTP_200_OK,
+            tags=["posts"])
+async def delete_post(post_id: int) -> dict[str, str]:
+    for i, post in enumerate(posts_db):
+        if post.id == post_id:
+            del posts_db[i]
+            return {"detail": f"Post {post_id} was deleted!"}
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="Post not found"
+    )
 
-    return f"Post {post_id} was successfully deleted"
 
 @app.delete("/posts", status_code=status.HTTP_200_OK, tags=["posts"])
-async def delete_posts() -> str:
+async def delete_posts() -> dict[str, str]:
     posts_db.clear()
-    return "All posts was successfully deleted"
+    return {"detail": "All posts deleted!"}
