1ec99dec531f4819bc0e4f9e71cb6e155c7598db
leshakovalevskiy2002 <lesha_programmer02_gt@mail.ru>
2026-01-16 22:23:24 +0300

    feature: add depends to project

--- crud.py
+++ crud.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException, status
+from fastapi import FastAPI, HTTPException, status, Depends
 from pydantic import BaseModel, Field
 from typing import Annotated
 from datetime import datetime
@@ -57,6 +57,18 @@ class Post(BaseModel):
 posts_db: list[Post] = []
 
 
+def get_post_or_404(post_id: int) -> tuple[int, Post]:
+    for i, post in enumerate(posts_db):
+        if post.id == post_id:
+            return i, post
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="Post not found"
+    )
+
+FoundPost = Annotated[tuple[int, Post], Depends(get_post_or_404)]
+
+
 @app.get("/posts", tags=["posts"],
          response_model=list[Post])
 async def read_posts() -> list[Post]:
@@ -65,14 +77,8 @@ async def read_posts() -> list[Post]:
 
 @app.get("/posts/{post_id}", tags=["posts"],
          response_model=Post)
-async def read_post(post_id: int) -> Post:
-    for post in posts_db:
-        if post.id == post_id:
-            return post
-    raise HTTPException(
-        status_code=status.HTTP_404_NOT_FOUND,
-        detail="Post not found"
-    )
+async def read_post(found_post: FoundPost) -> Post:
+    return found_post[1]
 
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED,
@@ -88,31 +94,21 @@ async def create_post(new_post: PostUpsert) -> Post:
 @app.put("/posts/{post_id}", tags=["posts"],
          response_model=Post)
 async def update_post(
-        post_id: int,
+        found_post: FoundPost,
         updated_post: PostUpsert
 ) -> Post:
-    for i, post in enumerate(posts_db):
-        if post.id == post_id:
-            new_post = Post(id=post_id, date_created=post.date_created, **updated_post.model_dump())
-            posts_db[i] = new_post
-            return new_post
-    raise HTTPException(
-        status_code=status.HTTP_404_NOT_FOUND,
-        detail="Post not found"
-    )
+    index, post = found_post
+    new_post = Post(id=post.id, date_created=post.date_created, **updated_post.model_dump())
+    posts_db[index] = new_post
+    return new_post
 
 
 @app.delete("/posts/{post_id}", status_code=status.HTTP_200_OK,
             tags=["posts"])
-async def delete_post(post_id: int) -> dict[str, str]:
-    for i, post in enumerate(posts_db):
-        if post.id == post_id:
-            del posts_db[i]
-            return {"detail": f"Post {post_id} was deleted!"}
-    raise HTTPException(
-        status_code=status.HTTP_404_NOT_FOUND,
-        detail="Post not found"
-    )
+async def delete_post(found_post: FoundPost) -> dict[str, str]:
+    index, post = found_post
+    del posts_db[index]
+    return {"detail": f"Post {post.id} was deleted!"}
 
 
 @app.delete("/posts", status_code=status.HTTP_200_OK, tags=["posts"])
